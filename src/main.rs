83e0785ef3ec830262569015f04ac381cc2371b3
Aaron Signer <aaronsigner1995@proton.me>
2025-09-23 16:40:20 -0400

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use rusqlite::{Connection, Result, params}; // For database operations and result handling
+
+#[derive(Debug)]
+struct Game {
+    game_id: String,
+    player_1: Option<String>,
+    player_2: Option<String>,
+}
+
+fn get_games(conn: Connection) -> Result<Vec<Game>> {
+    let mut stmt = conn.prepare("SELECT game_id, player_1, player_2 FROM games")?;
+    let rows = stmt.query_map([], |row| {
+        Ok(Game {
+            game_id: row.get(0)?,
+            player_1: row.get(1)?,
+            player_2: row.get(2)?,
+        })
+    })?;
+
+    let mut games = Vec::new();
+    for game in rows {
+        games.push(game?);
+    }
+
+    for game in &games {
+        println!(
+            "{:?}, {:?}, {:?}",
+            game.game_id, game.player_1, game.player_2
+        )
+    }
+
+    Ok(games)
+}
+
+fn main() {
+    let conn = connect_to_database("./../sqlite/chess_timers.db");
+    let conn2 = match conn {
+        Ok(connection) => connection,
+        Err(error) => panic!("Failed to connect to DB, error: {:}", error),
+    };
+
+    create_new_game(&conn2);
+
+    match get_games(conn2) {
+        Ok(game) => println!("Got games"),
+        Err(error) => println!("Failed to get games: {:}", error),
+    }
+}
+
+fn create_new_game(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "INSERT INTO games (game_id, player_1) VALUES (?1, ?2)",
+        params!["game2", "Aaron"], // Bind parameters
+    )?;
+
+    println!("User inserted successfully.");
+    Ok(())
+}
+
+fn connect_to_database(db_file_path: &str) -> Result<Connection> {
+    let conn = Connection::open(db_file_path);
+    conn
+}
